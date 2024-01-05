66efa3a87df5fdf7c4bcdd29afe83bd2512056e3
RaziqDin <144256646+nidqija@users.noreply.github.com>
2024-01-05 22:01:19 +0800

    Create atm_data.py

---
+++ atm_data.py
@@ -0,0 +1,14 @@
+methods = ("1.Cash deposit" , 
+          "2.Cash Withdrawal" ,
+            "3.Pay" , )
+lines = ("-------------")
+
+tnb = 150
+air = 60
+astro = 130
+
+bills = ("1.tnb" 
+         , "2.air" ,
+           "3.astro")
+
+
