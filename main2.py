7c480ac142309b9ca7f6cd9a48491bacaff84c65
RaziqDin <144256646+nidqija@users.noreply.github.com>
2024-01-28 15:29:12 +0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -12,96 +12,196 @@ print('------------------------')
 print('------------------------')
 
 
+
+
+
+
+
+
+      
+
+
 Valid = True
 
-while Valid:
-         
- passkey = int(input('Enter Passkey:'))
-
- for row in sheet.rows:
-        if row[1].value == passkey:
-            print(f'Hello ' , row[0].value)
-            Valid = False
 
 
 
-            for method in atm_data.methods:
-               print(method)
-               print('-----------------------------')
+
+
+passkey = int(input('Enter Passkey:'))
+
+
+while Valid:  
+
+       for row in sheet.rows:
+          if row[1].value == passkey:
+               print(f'Hello ' , row[0].value)
+               Valid = False
+               
+
+
+
+               for method in atm_data.methods:
+                  print(method)
+                  print('-----------------------------')
    
 
 
-            choice = int(input("Enter Choice:"))
-
-            if choice == 1:
-                 print(f'Your current balance is:' ,row[2].value)
-                 deposit = float(input("Enter Your Deposit:"))
-                 new_deposit = row[2].value + deposit
-                 row[2].value = new_deposit
-                 print(f'Thank You! , Your new balance is : RM' , row[2].value)
-                 wb.save('raziq.xlsx')
+               choice = int(input("Enter Choice:"))
+ 
+               if choice == 1:
+                        print(f'Your current balance is:' ,row[2].value)
+                        deposit = float(input("Enter Your Deposit:"))
+                        new_deposit = row[2].value + deposit
+                        row[2].value = new_deposit
+                        print(f'Thank You! , Your new balance is : RM' , row[2].value)
+                        wb.save('raziq.xlsx')
        
       
 
 
-            elif choice == 2:
-                deduc = True
-                while deduc:
-                     print(f'Your current balance is:' ,row[2].value)
-                     deduction = float(input("Enter Your Withdrawal Amount:"))
+               elif choice == 2:
+                    deduc = True
+                    while deduc:
+                          print(f'Your current balance is:' ,row[2].value)
+                          deduction = float(input("Enter Your Withdrawal Amount:"))
       
-                     if deduction <= row[2].value:
-                          new_balance = row[2].value - deduction
-                          row[2].value = new_balance
-                          print(f'Thank You! , Your new balance is : RM' , row[2].value)
-                          wb.save('raziq.xlsx')
-                          deduc = False
+                          if deduction <= row[2].value:
+                                 new_balance = row[2].value - deduction
+                                 row[2].value = new_balance
+                                 print(f'Thank You! , Your new balance is : RM' , row[2].value)
+                                 wb.save('raziq.xlsx')
+                                 deduc = False
      
 
-                     else :
-                          print("Withdraw cannot exceed the intended balance!")
+                          else :
+                               print("Withdraw cannot exceed the intended balance!")
 
 
 
 
 
-            elif choice == 3:
-                    print('These are your current bill:')
-                    print(f'TNB bill : RM' , row[3].value)
-                    print(f'Water bill : RM' , row[4].value)
-                    print(f'Astro bill : RM' ,row[5].value)
-                    bill = str(input('Enter Bill Type to pay:'))
+               elif choice == 3:
+                        print('These are your current bill:')
+                        print(f'TNB bill : RM' , row[3].value)
+                        print(f'Water bill : RM' , row[4].value)
+                        print(f'Astro bill : RM' ,row[5].value)
+                        bill = str(input('Enter Bill Type to pay:'))
                     
 
-                    if bill == 'TNB':
-                         print(f'TNB bill : RM' , row[3].value)
-                         amount = float(input('Enter Amount: RM'))
-                         new_amount = row[3].value - amount
-                         row[3].value = new_amount
-                         print(f"Thank You! , Your Current Bill is : RM" , row[3].value)
-                         wb.save('raziq.xlsx')
+                        if bill == 'TNB':
+                             print(f'TNB bill : RM' , row[3].value)
+                             amount = float(input('Enter Amount: RM'))
+                             new_amount = row[3].value - amount
+                             row[3].value = new_amount
+                             print(f"Thank You! , Your Current Bill is : RM" , row[3].value)
+                             wb.save('raziq.xlsx')
+  
+
+                        elif bill == 'Water':
+                             print(f'Water bill : RM' , row[4].value)
+                             amount = float(input('Enter Amount: RM'))
+                             new_amount2 = row[4].value - amount
+                             row[4].value = new_amount2
+                             print(f"Thank You! , Your Current Bill is : RM" , row[4].value)
+
+                             wb.save('raziq.xlsx')
 
 
-                    elif bill == 'Water':
-                         print(f'Water bill : RM' , row[4].value)
-                         amount = float(input('Enter Amount: RM'))
-                         new_amount2 = row[4].value - amount
-                         row[4].value = new_amount2
-                         print(f"Thank You! , Your Current Bill is : RM" , row[4].value)
+                        elif bill == 'Astro':
+                             print(f'Water bill : RM' , row[5].value)
+                             amount = float(input('Enter Amount: RM'))
+                             new_amount3 = row[5].value - amount
+                             row[5].value = new_amount3
+                             print(f"Thank You! , Your Current Bill is : RM" , row[5].value)
+                             wb.save('raziq.xlsx')
 
-                         wb.save('raziq.xlsx')
 
 
-                    elif bill == 'Astro':
-                         print(f'Water bill : RM' , row[5].value)
-                         amount = float(input('Enter Amount: RM'))
-                         new_amount3 = row[5].value - amount
-                         row[5].value = new_amount3
-                         print(f"Thank You! , Your Current Bill is : RM" , row[5].value)
-                         wb.save('raziq.xlsx')
-                         
-else:
-        print("Invalid Passkey , Please try again")
+       again =str(input("Run again ? (y/n):"))
+ 
+
+       if 'y' in again:
+                 
+                  Valid = True
+ 
+       else:
+                  print("Thank you for using our service , see you soon!")
+                  Valid = False
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+           
+              
+
+
+
+
+
+
+
+
+
+
+
+
+                 
+
+
+
+  
+
+      
+
+
+  
+      
+      
+
+    
+
+
+
+     
+    
+     
+       
+
+            
+
+
+
+            
+            
+      
+
+
+
+       
+   
+    
+
+
+
+
+
+
+
+
 
 
 
