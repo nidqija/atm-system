c7a0e26e0c62abeefa3db5a1806e6f0f866c086f
RaziqDin <144256646+nidqija@users.noreply.github.com>
2024-01-30 15:32:03 +0800

    Update main2.py

--- main2.py
+++ main2.py
@@ -31,6 +31,7 @@ Valid = True
 passkey = int(input('Enter Passkey:'))
 
 
+
 while Valid:  
 
        for row in sheet.rows:
@@ -66,7 +67,7 @@ while Valid:
                           print(f'Your current balance is:' ,row[2].value)
                           deduction = float(input("Enter Your Withdrawal Amount:"))
       
-                          if deduction <= row[2].value:
+                          if deduction < row[2].value:
                                  new_balance = row[2].value - deduction
                                  row[2].value = new_balance
                                  print(f'Thank You! , Your new balance is : RM' , row[2].value)
@@ -83,38 +84,64 @@ while Valid:
 
                elif choice == 3:
                         print('These are your current bill:')
-                        print(f'TNB bill : RM' , row[3].value)
-                        print(f'Water bill : RM' , row[4].value)
-                        print(f'Astro bill : RM' ,row[5].value)
+                        print(f' 1. TNB bill : RM' , row[3].value)
+                        print(f' 2. Water bill : RM' , row[4].value)
+                        print(f' 3. Astro bill : RM' ,row[5].value)
                         bill = str(input('Enter Bill Type to pay:'))
                     
 
-                        if bill == 'TNB':
+                        if bill == '1':
                              print(f'TNB bill : RM' , row[3].value)
                              amount = float(input('Enter Amount: RM'))
                              new_amount = row[3].value - amount
                              row[3].value = new_amount
                              print(f"Thank You! , Your Current Bill is : RM" , row[3].value)
                              wb.save('raziq.xlsx')
+
+                             if row[3].value == 0:
+                                    print('Congratulations ! your bill is fully paid!')
+
+
   
 
-                        elif bill == 'Water':
+                        elif bill == '2':
                              print(f'Water bill : RM' , row[4].value)
                              amount = float(input('Enter Amount: RM'))
                              new_amount2 = row[4].value - amount
                              row[4].value = new_amount2
                              print(f"Thank You! , Your Current Bill is : RM" , row[4].value)
-
                              wb.save('raziq.xlsx')
 
+                             if row[4].value == 0:
+                                    print('Congratulations ! your bill is fully paid!')
 
-                        elif bill == 'Astro':
+
+                        elif bill == '3':
                              print(f'Water bill : RM' , row[5].value)
                              amount = float(input('Enter Amount: RM'))
                              new_amount3 = row[5].value - amount
                              row[5].value = new_amount3
                              print(f"Thank You! , Your Current Bill is : RM" , row[5].value)
                              wb.save('raziq.xlsx')
+
+                             if row[5].value == 0:
+                                     print('Congratulations ! your bill is fully paid!')
+                                    
+
+
+
+               elif choice == 4:
+                           print(f'Registered Name:' , row[0].value)
+                           print(f'Balance: RM' , row[2].value)
+                           print('Bills :')
+                           print('------------------')
+                           print('------------------')
+                           print(f'TNB BILL : RM' , row[3].value)
+                           print(f'WATER BILL : RM' , row[4].value)
+                           print(f'ASTRO BILL : RM' , row[5].value)
+
+               else:
+                      print('Invalid Choice , Please Try Again')
 
 
 
@@ -128,6 +155,80 @@ while Valid:
        else:
                   print("Thank you for using our service , see you soon!")
                   Valid = False
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+           
+              
+
+
+
+
+
+
+
+
+
+
+
+
+                 
+
+
+
+  
+
+      
+
+
+  
+      
+      
+
+    
+
+
+
+     
+    
+     
+       
+
+            
+
+
+
+            
+            
+      
+
+
+
+       
+   
+    
+
+
+
+
+
+
+
+
 
 
 
