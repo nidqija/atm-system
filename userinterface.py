62a9496ffdd1306c73a51ed2808f133811af4f89
RaziqDin <144256646+nidqija@users.noreply.github.com>
2024-02-20 20:17:12 +0800

    Update userinterface.py

--- userinterface.py
+++ userinterface.py
@@ -1,53 +1,63 @@
-import openpyxl
-import atm_data
 import tkinter as tk
-import ttkbootstrap as ttk
-from atm_data import methods
 
-root = tk.Tk()
-root.geometry('800x600')
-root.title('ATM System')
-root.configure(bg='blue')
-button = tk.Button(root , text = 'Start' , font=('Helvetica' , 20) ,height=1 , width= 30 , bg='yellow' , command=lambda:home_page())
-button.pack(padx = 10 , pady= 20)
+class GUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)   
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        
+
+        self.frames = {}
+        for F in (loginpage , welcomepage ):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+           
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("loginpage")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
-
-
-def home_page():
-    global label
-    home_frame = tk.Frame(root)
-    title = tk.Label(root , text='Welcome to XYZ Bank!' , font=('Helvetica' , 35 , 'bold' ,) , bg='blue' , fg='yellow' )
-    title.pack(padx= 20 , pady= 30)
-    label = tk.Entry(root , text = 'Enter Account Number:' , font=('Helvetica' , 25) , bg='white' )
-    label.pack()
-    button = tk.Button(root , text = 'Submit' , font=('Helvetica' , 20) ,height=1 , width= 30 , bg='yellow' , command=lambda:choice_page())
-    button.pack(padx = 10 , pady= 20)
-    home_frame.pack()
-
-
-def choice_page():
-    global wb
-    global sheet
-    global label
-    wb = openpyxl.load_workbook('raziq.xlsx')
-    sheet = wb.active
-    account_number = int(label.get())
-    choice_frame = tk.Frame(root)  
-    for row in sheet.rows:
-        if account_number == row[1].value:
-            welcome_label = tk.Label(choice_frame , text = f'Welcome! {row[0].value}' , fg='black' , font=('Helvetica' , 30))
-            welcome_label.pack(padx=20 ,pady=20 ,side='top')
-
+class loginpage(tk.Frame):
     
-    choice_frame.pack()
-      
+    def __init__(self, parent , controller):
+        tk.Frame.__init__(self , parent)
+        self.controller = controller
+        self.configure(background='blue')
+        title = tk.Label(self , text='Welcome to XYZ Bank!' , font=('Helvetica' , 35 , 'bold' ,) , bg='blue' , fg='yellow')
+        title.pack(padx= 20 , pady= 30)
+        label = tk.Entry(self , text = 'Enter Account Number:' , font=('Helvetica' , 25) , bg='white' )
+        label.pack()
+        button = tk.Button(self , text = 'Submit' , font=('Helvetica' , 20) ,height=1 , width= 30 , bg='yellow' ,command=lambda:controller.show_frame('welcomepage'))
+        button.pack(padx = 10 , pady= 20)
 
 
-             
-
-      
+class welcomepage(tk.Frame):
+     def __init__(self, parent , controller):
+        tk.Frame.__init__(self , parent)
+        self.configure(background='blue')
+        self.controller = controller
+        title = tk.Label(self , text='Hello' , font=('Helvetica' , 35 , 'bold' ,) , bg='blue' , fg='yellow' )
+        title.pack(padx= 20 , pady= 30)
+     
     
 
-root.mainloop()
 
+
+
+
+
+
+if __name__ == "__main__":
+    app = GUI()
+    app.geometry('600x500')
+    app.mainloop()
